d098ec6d906ef0eed152a1eaa25c8ba20d4d99f8
Haris Colic <colicharis1@gmail.com>
2024-09-27 23:22:04 +0200

    test: fixes unit tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,6 +1,9 @@
 from app.services import analyse_sentiment
+from unittest.mock import MagicMock
 
 def test_analyse_sentiment():
-    sentiments = analyse_sentiment('The stock market is volatile.')
+    mock_pipeline = MagicMock()
+    mock_pipeline.return_value = [[{'label': 'LABEL_2', 'score': 0.9817743301391602}, {'label': 'LABEL_1', 'score': 0.014286653138697147}, {'label': 'LABEL_0', 'score': 0.003938947804272175}]]
+    sentiments = analyse_sentiment('The stock market is volatile.', sentiment_pipeline=mock_pipeline)
     assert len(sentiments) > 0
     assert sentiments[0].label in ['LABEL_0', 'LABEL_1', 'LABEL_2']
