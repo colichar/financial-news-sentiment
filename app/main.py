9b636e3268195aeea823d82e475b587f46a88ee7
Haris Colic <colicharis1@gmail.com>
2024-09-29 18:43:52 +0200

    fix: use mangum as wrapper for fastapi app for lambda

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from transformers import pipeline, AutoModelForSequenceClassification, AutoToken
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 import os
+from mangum import Mangum
 
 load_dotenv()
 
@@ -33,3 +34,6 @@ app.include_router(sentiment_router)
 @app.get('/health')
 async def health_check():
     return {'status': 'ok'}
+
+
+handler = Mangum(app)
